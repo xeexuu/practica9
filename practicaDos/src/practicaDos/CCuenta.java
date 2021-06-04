0c6dd9cf6c8141192a2265624a06520353e9bb15
xeexuu_ <xeexuu_@xeexuu>
2021-06-04 17:28:03 +0200

    GITHUB Y ENLAZAR PROYECTO

---
+++ practicaDos/src/practicaDos/CCuenta.java
@@ -0,0 +1,161 @@
+package practicaDos;
+
+/**
+ * @author Jesús Morgado Fernández
+ * @version 1.0
+ * @implNote Fecha: miércoles 02 de junio de 2021
+ */
+
+public class CCuenta {
+
+	/**
+	 * Atributos
+	 */
+	private String nombre;
+	private String cuenta;
+	private double saldo;
+	private double tipoInteres;
+
+	/**
+	 * Constructor por defecto
+	 */
+	public CCuenta() {
+	}
+
+	/**
+	 * Constructor parametrizado
+	 * 
+	 * @param nom  = nombre del usuario
+	 * @param cue  = nombre de la cuenta
+	 * @param sal  = saldo de la cuenta
+	 * @param tipo = tipo de interés
+	 */
+	public CCuenta(String nom, String cue, double sal, double tipo) {
+		nombre = nom;
+		cuenta = cue;
+		saldo = sal;
+		tipoInteres = tipo;
+	}
+
+	/**
+	 * Método que ingresa una cantidad
+	 * 
+	 * @param cantidad = cantidad a ingresar
+	 * @throws Exception = por si se produce un error, que el programa no se bloquee
+	 */
+	public void ingresar(double cantidad) throws Exception {
+		if (cantidad < 0)
+			throw new Exception("No se puede ingresar una cantidad negativa");
+		saldo = saldo + cantidad;
+	}
+
+	/**
+	 * Método que retira una cantidad
+	 * 
+	 * @param cantidad = cantidad a retirar
+	 * @throws Exception = por si se produce un error, que el programa no se bloquee
+	 */
+	public void retirar(double cantidad) throws Exception {
+		if (cantidad <= 0)
+			throw new Exception("No se puede retirar una cantidad negativa");
+		if (estado() < cantidad)
+			throw new Exception("No se hay suficiente saldo");
+		saldo = saldo - cantidad;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public String obtenerNombre() {
+		return getNombre();
+	}
+
+	/**
+	 * 
+	 * @param nombre
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	/**
+	 * 
+	 * @param nom
+	 */
+	public void asignarNombre(String nom) {
+		setNombre(nom);
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public String getCuenta() {
+		return cuenta;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public String obtenerCuenta() {
+		return getCuenta();
+	}
+
+	/**
+	 * 
+	 * @param cuenta
+	 */
+	public void setCuenta(String cuenta) {
+		this.cuenta = cuenta;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public double getSaldo() {
+		return saldo;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public double estado() {
+		return getSaldo();
+	}
+
+	/**
+	 * 
+	 * @param saldo
+	 */
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public double getTipoInteres() {
+		return tipoInteres;
+	}
+
+	/**
+	 * 
+	 * @param tipoInteres
+	 */
+	public void setTipoInteres(double tipoInteres) {
+		this.tipoInteres = tipoInteres;
+	}
+}
---
+++ practicaDos/src/practicaDos/Main.java
@@ -0,0 +1,36 @@
+package practicaDos;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		CCuenta cuentaUno;
+		double saldoActual;
+		cuentaUno = new CCuenta("Antonio López", "1000-2365-85-1230456789", 2500, 0);
+		saldoActual = cuentaUno.estado();
+		System.out.println("El saldo actual es" + saldoActual);
+		operacuenta(cuentaUno, 0);
+	}
+
+	/**
+	 * Método que llama a los métodos de la clase CCuenta para operar
+	 * 
+	 * @param cuentaUno = objeto de la clase CCuenta
+	 * @param cantidad  = cantidad con la que operar
+	 */
+	private static void operacuenta(CCuenta cuentaUno, float cantidad) {
+		try {
+			cuentaUno.retirar(2300);
+		} catch (Exception e) {
+			System.out.print("Fallo al retirar");
+		}
+		try {
+			System.out.println("Ingreso en cuenta");
+			cuentaUno.ingresar(695);
+		} catch (Exception e) {
+			System.out.print("Fallo al ingresar");
+		}
+	}
+}
